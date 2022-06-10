72a4233e6b562455c0a8ebf6a91f0139c22412c6
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-06-10 17:56:44 -0400

    !!! Fixed Silhouette Previous algorithm is incorrect. The graph looks pretty similiar and I suppose you can treat it as a faster, less accurate version of the algorithm, but it's nonetheless incorrect.

--- judging/silhouette.py
+++ judging/silhouette.py
@@ -1,5 +1,6 @@
 from genericpath import exists
-from distance import squared_dist_array
+from calculate import filter_dist_with_points
+from distance import squared_dist_array, squared_dist_p
 from plot import line_plot
 
 folder = "./results/"
@@ -8,8 +9,8 @@ def silhouette(ending, coords, folder = folder):
     centroid_count = []
     silhouette_scores = []
     
-    for i in range(2, 21):
-        path = folder + str(i) + ending
+    for z in range(2, 21):
+        path = folder + str(z) + ending
         if exists(path):
             with open(path) as file:
                 file_records = []
@@ -26,52 +27,69 @@ def silhouette(ending, coords, folder = folder):
                     values = "".join(values)
                     values = values.split(",")
                     centroids_list = []
-                    for j in range(0, len(values), 2):
-                        centroids_list.append((float(values[j]), float(values[j + 1])))
+                    for i in range(0, len(values), 2):
+                        centroids_list.append((float(values[i]), float(values[i + 1])))
                     file_centroids.append(centroids_list)
                     
                 if len(file_records) == 0:
                     continue;
                     
-                centroid_count.append(i)
+                centroid_count.append(z)
 
                 # Finding true record centroids
                 true_record = 999999
                 record_centroids = centroids_list[0]
-                for j, record in enumerate(file_records):
+                for i, record in enumerate(file_records):
                     if record < true_record:
                         true_record = record
-                        record_centroids = file_centroids[j]
+                        record_centroids = file_centroids[i]
                 
-                # Calculating A and B
+                # Score calculation
                 # Arrays (# centroids) of arrays (# points) of distances
                 dist_arrays = []
                 for centroid in record_centroids:
                     dist_arrays.append(squared_dist_array(centroid, coords))
-                # Best distances - We want second best
-                closest_dists = []
-                next_closest_dists = []
-                # For each point
-                for j in range(len(dist_arrays[0])):
-                    first_record = 9999999
-                    second_record = 9999999
-                    # For each centroid dist array
-                    for distance_array in dist_arrays:
-                        dist = distance_array[j]
-                        if dist < first_record:
-                            first_record = dist
-                    closest_dists.append(first_record)
-                    for distance_array in dist_arrays:
-                        dist = distance_array[j]
-                        if dist < second_record and dist != first_record:
-                            second_record = dist
-                    next_closest_dists.append(second_record)
-                
-                total_point_score = 0
-                for j in range(len(closest_dists)):
-                    first_record = closest_dists[j]
-                    second_record = next_closest_dists[j]
-                    total_point_score += (second_record - first_record) / second_record
-                silhouette_scores.append(total_point_score / len(closest_dists))
+                _, filtered_points = filter_dist_with_points(coords, dist_arrays)
+
+                point_dist_arrays = []
+                for point in coords:
+                    point_dist_arrays.append(squared_dist_array(point, record_centroids))
+
+                # For each point, find closest and second closest
+                #   For each point in closest, find distance to every
+                #   other point in second closest
+                true_total_score = 0
+                for i, point_centroid_dist in enumerate(point_dist_arrays):
+                    # Finding closest and second closest clusters
+                    closest = 99999999
+                    closest_index = 0
+                    next_closest = 99999999
+                    next_closest_index = 0
+                    
+                    for j, dist in enumerate(point_centroid_dist):
+                        if dist < closest:
+                            next_closest = closest
+                            next_closest_index = closest_index
+                            closest = dist
+                            closest_index = j
+                        elif dist < next_closest:
+                            next_closest = dist
+                            next_closest_index = j
+
+                    this_point = coords[i]
+                    avg_dist_to_closest = 0
+                    avg_dist_to_second = 0
+                    for cluster_point in filtered_points[closest_index]:
+                        dist_to_point = squared_dist_p(this_point, cluster_point)
+                        if dist_to_point == 0: continue
+                        avg_dist_to_closest += dist_to_point
+                    for next_cluster_point in filtered_points[next_closest_index]:
+                        dist_to_next = squared_dist_p(this_point, next_cluster_point)
+                        avg_dist_to_second += dist_to_next
+                    avg_dist_to_closest /= (len(filtered_points[closest_index]) - 1)
+                    avg_dist_to_second /= (len(filtered_points[next_closest_index]))
+                    
+                    true_total_score += (avg_dist_to_second - avg_dist_to_closest) / max(avg_dist_to_second, avg_dist_to_closest)
+                silhouette_scores.append(true_total_score / len(point_dist_arrays))
     
     line_plot(centroid_count, silhouette_scores)
---
+++ judging/silhouette_inc.py
@@ -0,0 +1,113 @@
+# NOTE: This algorithm is incorrect.
+
+from genericpath import exists
+from calculate import filter_dist_any
+from distance import squared_dist_array, squared_dist_p
+from plot import line_plot
+
+folder = "./results/"
+
+def silhouette(ending, coords, folder = folder):
+    centroid_count = []
+    silhouette_scores = []
+    
+    for i in range(2, 21):
+        path = folder + str(i) + ending
+        if exists(path):
+            with open(path) as file:
+                file_records = []
+                file_centroids = []
+                for line in file:
+                    values = line.split(" ")
+                    file_records.append(float(values[0]))
+                    values = "".join(values[1 : ])
+                    values = values.strip()
+                    values = values[1 : len(values) - 1]
+                    values = values.split("(")
+                    values = "".join(values)
+                    values = values.split(")")
+                    values = "".join(values)
+                    values = values.split(",")
+                    centroids_list = []
+                    for j in range(0, len(values), 2):
+                        centroids_list.append((float(values[j]), float(values[j + 1])))
+                    file_centroids.append(centroids_list)
+                    
+                if len(file_records) == 0:
+                    continue;
+                    
+                centroid_count.append(i)
+
+                # Finding true record centroids
+                true_record = 999999
+                record_centroids = centroids_list[0]
+                for j, record in enumerate(file_records):
+                    if record < true_record:
+                        true_record = record
+                        record_centroids = file_centroids[j]
+                
+                dist_arrays = []
+                for centroid in record_centroids:
+                    dist_arrays.append(squared_dist_array(centroid, coords))
+                filtered_dists = filter_dist_any(dist_arrays)
+                
+                # Calculating A and B
+                closest_dists = []
+                next_closest_dists = []
+                for point_index, point in enumerate(coords):
+                    first_record = 9999999
+                    first_record_centroid_index = 0
+                    second_record = 9999999
+
+                    for z, dist_array in enumerate(dist_arrays):
+                        squared_dist = dist_array[point_index]
+                        if squared_dist < first_record:
+                            second_record = first_record
+                            first_record = squared_dist
+                            first_record_centroid_index = z
+                        elif squared_dist < second_record:
+                            second_record = squared_dist
+
+                    first_record_centroid = record_centroids[first_record_centroid_index]
+                    # Hack to calculate mean of all other points in cluster:
+                    # Centroid is already mean, multiply by num of points,
+                    # subtract self, divide by num of points - 1.
+                    
+                    num_points_assoc_centroid = len(filtered_dists[first_record_centroid_index])
+                    cent_x, cent_y = first_record_centroid
+                    # print(cent_x, cent_y)
+                    cent_x = cent_x * num_points_assoc_centroid
+                    cent_y = cent_y * num_points_assoc_centroid
+                    # print(cent_x, cent_y)
+                    cent_x = cent_x - point[0]
+                    cent_y = cent_y - point[1]
+                    # print(cent_x, cent_y)
+                    cent_x = cent_x / (num_points_assoc_centroid - 1)
+                    cent_y = cent_y / (num_points_assoc_centroid - 1)
+                    # print(cent_x, cent_y)
+                    # print()
+                    # print()
+                    # print()
+                    # print()
+                    # print()
+                    # print()
+                    
+                    # print(first_record_centroid)
+                    # print(num_points_assoc_centroid)
+                    # print(cent_x, cent_y)
+                    # print(point)
+                    # print(first_record)
+                    # print(squared_dist_p(point, (cent_x, cent_y)))
+                    # print(second_record)
+                    # return;
+                    closest_dists.append(squared_dist_p(point, (cent_x, cent_y)))
+                    next_closest_dists.append(second_record)
+
+                total_point_score = 0
+                for j in range(len(closest_dists)):
+                    first_record = closest_dists[j]
+                    second_record = next_closest_dists[j]
+                    total_point_score += (second_record - first_record) / max(second_record, first_record)
+                silhouette_scores.append(total_point_score / len(closest_dists))
+    
+    line_plot(centroid_count, silhouette_scores)
